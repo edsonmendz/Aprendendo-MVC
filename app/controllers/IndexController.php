b08d9c3cfeda88cb3281b9f095db84b88df0e677
edsonmendz <japaact@gmail.com>
2023-06-24 19:31:19 -0300

    Algum problema na rota

---
+++ app/controllers/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+    namespace App\controllers;
+
+    class IndexController {
+
+        public function index() {
+            echo 'Chegamos ao indexController e disparamos a action index';
+        }
+
+        public function sobreNos() {
+            echo 'Chegamos ao indexController e disparamos a action sobreNos';
+        }
+    }
+
+?>
