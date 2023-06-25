e8caaee86e25f439017212fe00b7afb1ef3d64a3
edsonmendz <japaact@gmail.com>
2023-06-24 23:39:34 -0300

    Reutilizando layout

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -2,33 +2,22 @@
 
     namespace App\Controllers;
 
-    class IndexController {
+    use MF\Controller\Action;
 
-        private $view;
-
-        public function __construct() {
-            $this->view = new \stdClass();
-        }
+    class IndexController extends Action{
 
         public function index() {
 
             $this->view->dados = array('Sofá', 'Cadeira', 'Cama');
-            $this->render('index');
+            $this->render('index', 'layout2');
         }
 
         public function sobreNos() {
             $this->view->dados = array('Notebook', 'Smartphone');
-            $this->render('sobreNos');
+            $this->render('sobreNos', 'layout2');
         }
 
-        public function render($view) {
-            $classeAtual = get_class($this);
-
-            $classeAtual = str_replace('App\\Controllers\\', '', $classeAtual);
-            $classeAtual = strtolower(str_replace('Controller', '', $classeAtual ));
-
-            require_once "../App/views/".$classeAtual."/".$view.".phtml";
-        }
+        
     }
 
     
---
+++ vendor/MF/Controller/Action.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace MF\Controller;
+
+abstract class Action {
+   
+
+    protected $view;
+    public function __construct() {
+        $this->view = new \stdClass();
+    }
+
+    protected function render($view, $layout) {
+        $this->view->page = $view;
+
+        if (file_exists("../app/views/".$layout.".phtml")) {
+        require_once "../app/views/".$layout.".phtml";
+        } else {
+            $this->content();
+        }
+    }
+
+    protected function content() {
+        $classeAtual = get_class($this);
+
+        $classeAtual = str_replace('App\\Controllers\\', '', $classeAtual);
+        $classeAtual = strtolower(str_replace('Controller', '', $classeAtual ));
+
+        require_once "../App/views/".$classeAtual."/".$this->view->page.".phtml";
+    }
+}
